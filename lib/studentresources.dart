c1e71565a7843b0a76b3a1d2297db60c21d4068c
jayhawker6 <andrewgguernsey@gmail.com>
2020-12-05 14:29:42 -0600

    Resized all assets to be 300x300 and made all but the 'none' asset a PNG

--- lib/studentresources.dart
+++ lib/studentresources.dart
@@ -9,15 +9,15 @@ class StudentResources extends StatelessWidget {
         body: ListView(
           padding: const EdgeInsets.all(16.0),
           children: <Widget>[
-          this._buildListItem('StudentVue', 'https://parentvue.olatheschools.com/PXP2_Login_Student.aspx?regenerateSessionId=True', './studentvue.jpg'),
+          this._buildListItem('StudentVue', 'https://parentvue.olatheschools.com/PXP2_Login_Student.aspx?regenerateSessionId=True', './studentvue.png'),
             this._buildListItem('HomePage','https://www.olatheschools.org/','./homepage.png'),
-            this._buildListItem('Math Games', 'https://www.mathgames.com/', './mathgames.jpg'),
+            this._buildListItem('Math Games', 'https://www.mathgames.com/', './mathgames.png'),
             this._buildListItem('Google Classroom', 'https://classroom.google.com', './classroom.png'),
             this._buildListItem('ClassLink', 'https://launchpad.classlink.com/olatheusd233','./classlink.png'),
             this._buildListItem('Khan Academy','https://www.khanacademy.org/', './khanacademy.png'),
             this._buildListItem('Educational Videos', 'https://www.youtube.com/results?search_query=cat+videos','./youtube.png'),
-            this._buildListItem('Answer To Life And Everything', 'https://www.youtube.com/watch?v=oHg5SJYRHA0','./illuminati.jpg'),
-            this._buildListItem('IT Help Desk', 'https://www.ismycomputeronfire.com/','./ithelpdesk.jpg'),
+            this._buildListItem('Answer To Life And Everything', 'https://www.youtube.com/watch?v=oHg5SJYRHA0','./illuminati.png'),
+            this._buildListItem('IT Help Desk', 'https://www.ismycomputeronfire.com/','./ithelpdesk.png'),
     ]
     ));
   }
